dda63a222ffe744db8f34ec1e095098e3b69988d
Kyle Harrison <kyle.harrison344@gmail.com>
2022-04-29 10:42:12 -0400

    npx creat-react-app client folder added - file clean up done

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 5000
 const app = express()
 
 
-// app.use('/', (req, res) => {
-//     res.status(200).json({ message: 'testing api routes'})
-// })
-
 app.use('/api/test', require('./routes/testRoutes'))
+
+
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`) )
 
+ //npm run server
