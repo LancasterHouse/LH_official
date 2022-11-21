138d0c2929cfe5985a94f39fa93f67806ddd04c7
Kyle Harrison <kyle.harrison344@gmail.com>
2022-11-21 14:53:06 -0500

    css added to landing page

--- client/src/components/ProfessionalLP.js
+++ client/src/components/ProfessionalLP.js
@@ -1,6 +1,34 @@
+// import { Image } from 'react-bootstrap';
+// import PLPImage1 from "../assets/images/PLP/plp1.jpg";
+// import PLPImage2 from "../assets/images/PLP/plp2.png";
+
+// import React from "react";
+
+// export default function PLP() {
+//     return (
+//       <>
+
+//         <div className='plp-text-box'>
+//             <h1>Professional Learning Programs</h1>
+//             <Image src={PLPImage1} />
+//             <Image src={PLPImage2} />
+
+//         </div>
+        
+        
+     
+       
+
+//       </>
+//     );
+//   }
+
+
+
+
 import { Image } from 'react-bootstrap';
-import PLPImage1 from "../assets/images/PLP/plp1.jpg";
-import PLPImage2 from "../assets/images/PLP/plp2.png";
+//import PLPImage1 from "../assets/images/PLP/plp1.jpg";
+// import PLPImage2 from "../assets/images/PLP/plp2.png";
 
 // import PLPImage2 from "../assets/images/PLP/plp2.jpg";
 import React from "react";
@@ -11,9 +39,11 @@ export default function PLP() {
 
         <div className='plp-text-box'>
             <h1>Professional Learning Programs</h1>
-            <Image src={PLPImage1} />
-            <Image src={PLPImage2} />
-
+            <div className='plp-box-LRC'>
+            <p>Labour Relations</p>
+            <p>Video Preview</p>
+            </div>
+          
         </div>
         
         
